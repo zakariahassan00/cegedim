aa8f6e8dd773b983b03efc6918cfbeea6235a263
Sevrain <sevrain.chea@maiia.com>
2021-02-15 15:51:31 +0100

    add practitionerId to filter availabilities

--- pages/api/availabilities.ts
+++ pages/api/availabilities.ts
@@ -5,11 +5,9 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     console.log(req.query)
     const prisma = new PrismaClient()
-    const availabilities = await prisma.availability.findMany(
-        //{
-        //where: {practitionerId: +req.query.practitionerId},
-    //}
-    )
+    const availabilities = await prisma.availability.findMany({
+        where: {practitionerId: +req.query.practitionerId},
+    })
 
     res.status(200).json(availabilities)
   }
